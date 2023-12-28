916721b04d2ef6590c2dd54c05796db54cb92cd9
viacheslavk55 <525vkostenko@frhsd.com>
2023-12-28 11:28:02 -0500

    finished product

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function generateMadLib() {
     let adjective = prompt('Enter an Adjective:')
-    let noun = prompt('Enter a Noun:')
     let verb = prompt('Enter a Verb:')
     let adverb = prompt('Enter an Adverb:')
 
-    let story = `At the North Pole, the elves ${adverb} ${verb} around, accusing each other of hiding the ${adjective} and ${noun} gifts.`
+    let story = `At the North Pole, the elves ${adverb} ${verb} around, accusing each other of hiding the ${adjective} gifts.`
 
     let madLibOutputDiv = document.getElementById('madLibOutput')
 
